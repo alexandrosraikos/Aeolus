f221382c7968865d4f55c7c9545b1f81a136b66f
Alexandros Raikos <alexandros@araikos.gr>
2021-02-02 15:15:27 +0000

    init

---
+++ nauarchos/gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "Nauarchos",
+  },
+  plugins: [
+    "gatsby-plugin-sass",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-offline",
+  ],
+};
